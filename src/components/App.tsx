74f969a7435edf3670bbc3f91969026410ae2686
dmmi0917 <dmitry.mikhaylov@netcracker.com>
2021-09-10 12:36:58 +0300

    Init app

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+
+export const App: React.FC = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        setInterval(() => {
+            setDate(new Date());
+        }, 1000)
+    }, []);
+
+    return (
+        <div>Hello world! Time: {date.getHours()}:{date.getMinutes()}:{date.getSeconds()}</div>
+    );
+};
